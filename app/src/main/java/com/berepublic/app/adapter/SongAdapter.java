7183942a5b3cbebd026042833b197c65b2ac7e19
Dani Santeugini <dani@Mac-mini-de-Dani.local>
2016-05-10 10:40:29 +0200

    Deleted SongHolder

--- app/src/main/java/com/berepublic/app/adapter/SongAdapter.java
+++ app/src/main/java/com/berepublic/app/adapter/SongAdapter.java
@@ -9,7 +9,6 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.berepublic.app.R;
-import com.berepublic.app.adapter.holder.SongHolder;
 import com.berepublic.app.model.Song;
 import com.facebook.drawee.view.SimpleDraweeView;
 
